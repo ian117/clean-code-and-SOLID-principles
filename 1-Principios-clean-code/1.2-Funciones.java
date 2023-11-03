2fbc98d8ed9f3a5af925e230b7994b62fb093ba3
Ian Vassallo <spideramerica71@gmail.com>
2023-11-03 00:04:13 -0600

    lessons: added previous lessons

---
+++ 1-Principios-clean-code/1.2-Funciones.java
@@ -0,0 +1,15 @@
+// Claras, Concisas, Faciles de leer y modificar
+
+/**
+ *  Pequeñas
+ 
+ *  Hacen una sola cosa
+ 
+ *  Nivel de abstracción Única
+
+ *  Reciben pocos argumentos
+
+ *  No tienen efectos secundarios
+ 
+ *  Devuelven excepciones, NO códigos de error
+ */
