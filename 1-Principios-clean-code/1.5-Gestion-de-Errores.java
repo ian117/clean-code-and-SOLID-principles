83b47e4bde41cbc2021d841cb61562bf0d1de261
Ian Vassallo <spideramerica71@gmail.com>
2023-11-03 23:03:44 -0600

    lesson: Gestion de errores

---
+++ 1-Principios-clean-code/1.5-Gestion-de-Errores.java
@@ -0,0 +1,16 @@
+/**
+ 
+ * Código debe ser limpio, pero robusto
+ 
+ * Can be messy, that's why needs tobe structured
+  
+
+ *   COSAS A APLICAR *
+
+    > Devolver excepciones en lugar de códigos de error
+
+    > Usar excepciones Unchecked (Java)
+
+    > No devolver ni pasar Null
+
+ */
