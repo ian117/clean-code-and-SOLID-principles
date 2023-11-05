7f496dff4cf62c84a85890b44fa39cb8ed828218
Ian Vassallo <spideramerica71@gmail.com>
2023-11-05 16:29:20 -0600

    lessons: saved

---
+++ 2-Analizando-Code-SMELLS/1-Code-Smells.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 
+ *  Son síntomas de que el código no es todo lo limpio que debería
+ * 
+ *  "Algo huele mal en el código"
+ * 
+ *  7 Tipos de Code Smells
+ * 
+ *   ° En comentarios
+ * 
+ *   ° Entorno de Desarrollo
+ * 
+ *   ° En Funciones
+ * 
+ *   ° Code Smells Generales 
+ *    
+ *   ° En nombres
+ * 
+ *   ° En Test
+ */
