35cd66f8520d0fc6807c7634aba45a75d3a98b91
Ian Vassallo <spideramerica71@gmail.com>
2023-11-04 21:13:37 -0600

    lesson: Test 1.6

---
+++ 1-Principios-clean-code/1.6-Test-Unitarios.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * 
+ *   > Fundamental para refactorizar código ✅
+ * 
+ *   > Tener cobertura alta (>90%) ofrece gran seguridad a la hora de hacer modificaciones en las funciones ✅
+ * 
+ */
+
+
+//      "Si no funciona, no lo toques" ❌
+//      Esto es un síntoma de que faltan Test
