242a81a75e4de62f2ec0a9e63fdecadfc337fdac
Ian Vassallo <spideramerica71@gmail.com>
2023-11-06 12:40:40 -0600

    lesson: code smell en entorno de desarrollo

---
+++ 1.2-Code-Smells-Entorno-de-desarrollo.java
@@ -0,0 +1,16 @@
+/**
+ * Compilación requiere más de un paso ✅
+ */
+    // Debe ser capaz de hacer un checkout del código fuente y compilar o correr con otro
+
+    git clone route
+
+    mvn install ||  npm install && npm run start 
+
+/**
+ * Test requiere más de un paso ✅
+ */
+
+    // Los test se deben de ejecutar con un comando rápido y obvio
+
+    // Peligro de no ejecutar los test habitualmente por pereza
