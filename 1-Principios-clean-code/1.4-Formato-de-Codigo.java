c38fdc9aad4a436f6e642e6e3f1b2a4d1924cfd8
Ian Vassallo <spideramerica71@gmail.com>
2023-11-12 23:09:35 -0600

    fix: removed unused files

--- 1-Principios-clean-code/1.4-Formato-de-Codigo.java
+++
@@ -1,8 +0,0 @@
-/**
- * Se ven 2 Formatos
- *                 - Vertical    >  Densidad, Distancia y Orden
- *                 - Horizontal  >  Densidad e identación
- */
-
-// Se puede seguir la guía de Java de Google por dar un ejemplo
-// https://google.github.io/styleguide/javaguide.html
